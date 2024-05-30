4dcb036c9e533ae1929edf877fad4f0c7826eede
avneeshgautam29 <gautamavneesh10@gmail.com>
2024-05-30 16:04:28 +0530

    update bleu score

--- bluescore.py
+++ bluescore.py
@@ -12,3 +12,4 @@ print('Individual 1-gram: %f' % sentence_bleu(reference, candidate, weights=(1, 
 print('Individual 2-gram: %f' % sentence_bleu(reference, candidate, weights=(0, 1, 0, 0)))
 print('Individual 3-gram: %f' % sentence_bleu(reference, candidate, weights=(0, 0, 1, 0)))  
 print('Individual 4-gram: %f' % sentence_bleu(reference, candidate, weights=(0, 0, 0, 1)))
+
