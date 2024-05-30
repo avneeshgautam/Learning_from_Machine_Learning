8aad02740c4d9251194ae47be6c145e862d6ae08
avneeshgautam29 <gautamavneesh10@gmail.com>
2024-05-30 15:58:15 +0530

    add code for bleu score

---
+++ bluescore.py
@@ -0,0 +1,13 @@
+from nltk.translate.bleu_score import sentence_bleu
+reference = [
+    'this is a dog'.split(),
+    'it is dog'.split(),
+    'dog it is'.split(),
+    'a dog, it is'.split() 
+]
+candidate = 'it is a dog'.split()
+
+print('Individual 1-gram: %f' % sentence_bleu(reference, candidate, weights=(1, 0, 0, 0)))
+print('Individual 2-gram: %f' % sentence_bleu(reference, candidate, weights=(0, 1, 0, 0)))
+print('Individual 3-gram: %f' % sentence_bleu(reference, candidate, weights=(0, 0, 1, 0)))  
+print('Individual 4-gram: %f' % sentence_bleu(reference, candidate, weights=(0, 0, 0, 1)))
