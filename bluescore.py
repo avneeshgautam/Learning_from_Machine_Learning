2eb457fb2759e32c0aad1eacb0d4eb396dc11675
avneeshgautam29 <gautamavneesh10@gmail.com>
2024-05-30 16:03:39 +0530

    update bleu code

--- bluescore.py
+++ bluescore.py
@@ -7,6 +7,7 @@ reference = [
 ]
 candidate = 'it is a dog'.split()
 
+# these are ngram code
 print('Individual 1-gram: %f' % sentence_bleu(reference, candidate, weights=(1, 0, 0, 0)))
 print('Individual 2-gram: %f' % sentence_bleu(reference, candidate, weights=(0, 1, 0, 0)))
 print('Individual 3-gram: %f' % sentence_bleu(reference, candidate, weights=(0, 0, 1, 0)))  
